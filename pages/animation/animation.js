28863f4278b43516325638d046507b17cbfea296
张照博 <1184827350@qq.com>
2017-02-06 20:19:26 +0800

    update 2017/2/6/20:18
    update 2017/2/6/20:18

--- pages/animation/animation.js
+++ pages/animation/animation.js
@@ -1,18 +1,62 @@
-Page({
-  onReady: function () {
-    this.animation = wx.createAnimation({
-  transformOrigin: "50% 50%",
-  duration: 1000,
-  timingFunction: "ease",
-  delay: 0
-})
+  function getRandomColor () {
+    let rgb = []
+    for (let i = 0 ; i < 3; ++i){
+      let color = Math.floor(Math.random() * 256).toString(16)
+      color = color.length == 1 ? '0' + color : color
+      rgb.push(color)
+    }
+    return '#' + rgb.join('')
+  } 
+
+  Page({
+    onReady: function () {
+      this.videoContext = wx.createVideoContext('myVideo')
+      this.animation = wx.createAnimation({
+    transformOrigin: "50% 50%",
+    duration: 1000,
+    timingFunction: "ease",
+    delay: 0
+  })
 
   },
+    inputValue: '',
 
 
   data:{
 // Chapters:[1,2,3,4,5,6,7,8,9,10,11,12],
-chapter:0
+chapter:0,
+ src: '',
+    danmuList: [
+      {
+        text: '请问YYW在吗？',
+        color: '#ff0000',
+        time: 1
+      },
+            {
+        text: '一个人写弹幕太苦逼了',
+        color: '#eeeeaa',
+        time: 2
+      },
+            {
+        text: '你们要的弹幕君来了，还差个计数君',
+        color: '#e0aaa0',
+        time: 3
+      },
+            {
+        text: '傻大颜在不，能看到吗？？',
+        color: '#eeeeee',
+        time: 4
+      },
+            {
+        text: 'YYW是哪个？？',
+        color: '#faaaa0',
+        time: 5
+      },
+      {
+        text: '请问颜大傻是谁？',
+        color: '#ff00ff',
+        time: 6
+    }]
 },
 
 Next:function(){
@@ -80,9 +124,23 @@ this.setData(
     
    onShareAppMessage: function () {
     return {
-      title: '欢迎使用颜大傻牌音乐播放器',
-      desc: '将你正在听的歌分享到~~~',
+      title: '欢迎使用颜大傻牌视频播放器',
+      desc: '将你正在看的片分享到~~~',
       path: '/page/animation/animation.js'
     }
   },
+
+
+
+   bindInputBlur: function(e) {
+    this.inputValue = e.detail.value
+  },
+
+  bindSendDanmu: function () {
+    this.videoContext.sendDanmu({
+      text: this.inputValue,
+      color: getRandomColor()
+    })
+  }
+
 })
