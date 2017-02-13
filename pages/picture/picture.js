63d6b3495935f192cbeea94a22774ae0c5e811b8
张照博 <1184827350@qq.com>
2017-02-13 19:48:00 +0800

    update in library 19：47
    update in library 19：47

--- pages/picture/picture.js
+++ pages/picture/picture.js
@@ -27,8 +27,9 @@ source:"http://159.203.250.111/Carly.png",
   },
   
   yulan:function(){
+    var that=this;
     wx.previewImage({
-   current: 'http://119.29.74.46/myphoto/0.jpg', // 当前显示图片的链接，不填则默认为 urls 的第一张
+   current: 'http://119.29.74.46/myphoto/5.jpg', // 当前显示图片的链接，不填则默认为 urls 的第一张
   urls: [ 'http://119.29.74.46/myphoto/0.jpg',
    'http://119.29.74.46/myphoto/1.jpg',
     'http://119.29.74.46/myphoto/2.jpg',
@@ -49,6 +50,51 @@ source:"http://159.203.250.111/Carly.png",
   }
 })
   },
+
+
+Startrecord:function(){
+wx.startRecord({
+  success: function(res) {
+    var tempFilePath = res.tempFilePath
+      wx.playVoice({
+      filePath: tempFilePath
+    })
+
+    setTimeout(function() {
+        //暂停播放
+      wx.pauseVoice()
+    }, 5000)
+  }
+})
+
+
+    setTimeout(function(){
+      wx.stopVoice()
+    }, 5000)
+},
+
+
+
+Stoprecord:function(){
+    var that=this;
+   wx.stopRecord({
+    success: function(res){
+      // succes
+    },
+    fail: function() {
+      // fail
+    },
+    complete: function() {
+      // complete
+    }
+  })
+},
+
+
+
+
+
+
 
    onShareAppMessage: function () {
     return {
